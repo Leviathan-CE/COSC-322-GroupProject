c48e2ddb38f7fa2ac6c79b73382c91938273d8ac
Leviathan <leviathancreative@outlook.com>
2023-01-16 12:31:21 -0800

    minor fixes

--- src/main/java/ubc/cosc322/COSC322Test.java
+++ src/main/java/ubc/cosc322/COSC322Test.java
@@ -52,7 +52,7 @@ public class COSC322Test extends GamePlayer{
      * @param args for name and passwd (current, any string would work)
      */
     public static void main(String[] args) {				 
-    	COSC322Test player = new COSC322Test("password", "133");
+    	COSC322Test player = new COSC322Test(args[0], args[1]);
     	
     	if(player.getGameGUI() == null) {
     		player.Go();
