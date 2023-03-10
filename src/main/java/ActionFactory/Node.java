601f00b67077d60e0c314287a195d17f36198f52
Leviathan <leviathancreative@outlook.com>
2023-03-09 21:07:40 -0800

    Valuation of finding best node added

--- src/main/java/ActionFactory/Node.java
+++ src/main/java/ActionFactory/Node.java
@@ -12,11 +12,21 @@ public class Node extends GameBoardState {
 		
 		int id = 1;
 		static int count = 1;
-		double value;
+		
+		private double M =0; //monte-carlo value
+		private double h =0; //hueristic
+		private double g =0; //g-val
+		public double C = 1; //carlo constant
 		Node parent;
 		ArrayList<Node> children = new ArrayList<>();
 		
-	
+		
+		// F(n) = g+h+M 
+		public double getValue() {return (g+h)+(M*C);}
+		public void setUtilScore(double newVal) {M = newVal;} //monteCarlo value
+		public void setHueristicScore(double newVal) {h = newVal;}
+		public void setGval(double newVal){g=newVal;}
+		
 		public Node(ArrayList<Integer> gameBoard) {
 			super(gameBoard);
 			id= count++;
@@ -28,12 +38,14 @@ public class Node extends GameBoardState {
 		public String toString() {
 			return "" + id;
 		}
+
 		public void setParent(Node node) {
 			parent = node;
 		}
 		public void addChild(Node node) {
 			children.add(node);
-		}
+		}		
 		
+
 	
 }
