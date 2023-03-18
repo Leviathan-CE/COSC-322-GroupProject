fef0b092dfcfaf4753640dc224b055701aebdcbe
zachpren0 <77454945+zachpren0@users.noreply.github.com>
2023-03-17 19:15:23 -0700

    Revert "Merge branch 'testing-sweet' into Test_Action_Factory"
    This reverts commit d65e73498cb50cbafd68be1c00545206cc596494, reversing
    changes made to 30dc8d6398e406faadfa97bcb9acb25d90b973b4.

--- src/test/java/GameState/TestGameBoardState.java
+++ src/test/java/GameState/TestGameBoardState.java
@@ -10,35 +10,19 @@ import org.junit.jupiter.api.Test;
 public class TestGameBoardState {
 	
 	static GameBoardState board;
-	static  GameBoardState board10x10;
-	static GameBoardState boardOrientation;
-	static int[][] gameboard = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0},
-			             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0},
-			             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0},
-			             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0},
-			             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0},
-			             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0},
-			             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0},
-			             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0},
-			             {0, 1, 0, 0, 0, 0, 0, 0, 0, 0,0},
-			             {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0}};
 	
 	//Initialize a game board with no queens
 	@BeforeAll
 	public static void setup() {
 		ArrayList<Integer> startState = new ArrayList<>();
-		for(int x =0; x < GameBoardState.BORAD_DEMENSIONS;x++) {
-			for(int y = 0; y < GameBoardState.BORAD_DEMENSIONS;y++) {
+		for(int x =0; x < board.BOARD_WIDTH;x++) {
+			for(int y = 0; y < board.BOARD_HIEGHT;y++) {
 				startState.add(0);
 			}
 		}
 		board = new GameBoardState(startState);
-		
-		
-		
-		
 	}
-	//test to make sure setting a position and its value work within the range 
+	
 	@Test
 	public void TestsetPosValue() {
 		board.print();
@@ -57,39 +41,16 @@ public class TestGameBoardState {
 
 	}
 	
-	//test to make sure that the input for game board is a 11x11 board
-	//or reject the board otherwise
-	@Test
-	public void TestBoardDenensions() {
-		try {
-		board10x10 = new GameBoardState(new int[10][10]);
-		}catch(IndexOutOfBoundsException e) {
-			assertTrue(true);
-		}
-		assertTrue(board.getCurBoard()[0].length ==11);
-		
-		
-	
-	}
-	@Test
-	public void TestUpdateQueens() {
-		board.setPosValue(2,2, 2);
-		board.updateQueenPoses();
-		assertTrue(board.getQueenPosition2().size() == 1);
-		board.setPosValue(1, 3, 4);
-		board.updateQueenPoses();
-		assertTrue(board.getQueenPosition1().size() == 1);
-		board.setPosValue(3, 1, 3);
-		board.updateQueenPoses();
-		assertTrue(board.getArrowPositions().size() == 1);
-	}
-	@Test
-	public void testOrietation() {
-		boardOrientation = new GameBoardState(gameboard);
-		System.out.println("oreintation state");
-		boardOrientation.print();
-		assertTrue(boardOrientation.getCurBoard()[1][1] == 1);
-
-	}
+//	@Test
+//	public void TestSaveBoard() {
+//		board.SaveBoard();
+//		for(int x =0; x < board.BOARD_WIDTH;x++) {
+//			for(int y = 0; y < board.BOARD_HIEGHT;y++) {
+//				if(board.currentBoard[x][y] != board.savedBoard[x][y])
+//					assertTrue(false);
+//			}
+//		}
+//		assertTrue(true);
+//	}
 	
 }
