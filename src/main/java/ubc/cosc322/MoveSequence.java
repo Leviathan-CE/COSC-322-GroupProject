6874ca4dd2c45604c0c6588f1a801ef563a1fbc8
Leviathan <leviathancreative@outlook.com>
2023-03-20 17:33:48 -0700

    fixed bug with actionfactory

--- src/main/java/ubc/cosc322/MoveSequence.java
+++ src/main/java/ubc/cosc322/MoveSequence.java
@@ -91,10 +91,10 @@ public class MoveSequence {
 			count++;
 			int[] qs = n.countQueens();
 			if(qs[0] != 4 || qs[1] !=4) {
-			throw new IndexOutOfBoundsException("Queen count not accurate: Q1: "+qs[0]+";"+qs[1]+" in iteration: "+count);
+			throw new IndexOutOfBoundsException("Queen count not accurate: Q1: "+qs[0]+"; Q2;"+qs[1]+" in iteration: "+count);
 			}
 			n.updateQueenPoses();
-			n.C = Math.random() * 5;
+			n.C = Math.random() * 6;
 			n.setGval(n.h1());
 		}
 		return root;
@@ -117,7 +117,8 @@ public class MoveSequence {
 			}
 		}
 	}
-
+	
+	
 	public static ArrayList<ArrayList<Integer>> setSenderObj(int[] oldQ, int[] newQ, int[] arrw) {
 
 		ArrayList<ArrayList<Integer>> SenderOBJ = new ArrayList<>();
@@ -130,74 +131,6 @@ public class MoveSequence {
 		return SenderOBJ;
 	}
 
-//	// get queen position that changed between two states looking for the initial
-//	// state
-//	private static int[] getOldQueenPos(Node root, Node chosenMove, int QueenColor) {
-//		int[] oldQuen = new int[2];
-//		for (int i = 0; i < 4; i++) {
-//			// find oldqueen pos for black
-//			if (QueenColor == 1)
-//				if (root.getQueenPosition1().get(i)[0] != chosenMove.getQueenPosition1().get(i)[0]
-//						|| root.getQueenPosition1().get(i)[1] != chosenMove.getQueenPosition1().get(i)[1]) {
-//					oldQuen = root.getQueenPosition1().get(i);
-//					break;
-//				}
-//			// get old pos if white
-//			if (QueenColor == 2)
-//				if (root.getQueenPosition2().get(i)[0] != chosenMove.getQueenPosition2().get(i)[0]
-//						|| root.getQueenPosition2().get(i)[1] != chosenMove.getQueenPosition2().get(i)[1]) {
-//					oldQuen = root.getQueenPosition2().get(i);
-//					break;
-//				}
-//		}
-//		System.out.println("oldQinMeth: " + oldQuen[0] + ";" + oldQuen[1]);
-//		return oldQuen;
-//	}
-//
-//	// get queen position that changed between two states looking for the new
-//	// position.
-//	private static int[] getNewQueenPos(Node root, Node chosenMove, int QueenColor) {
-//		int[] newQuen = new int[2];
-//		for (int i = 0; i < 4; i++) {
-//			// find oldqueen pos for black
-//			if (QueenColor == 1)
-//				if (root.getQueenPosition1().get(i)[0] != chosenMove.getQueenPosition1().get(i)[0]
-//						|| root.getQueenPosition1().get(i)[1] != chosenMove.getQueenPosition1().get(i)[1]) {
-//					newQuen = chosenMove.getQueenPosition1().get(i);
-//					break;
-//				}
-//			// get old pos if white
-//			if (QueenColor == 2)
-//				if (root.getQueenPosition2().get(i)[0] != chosenMove.getQueenPosition2().get(i)[0]
-//						|| root.getQueenPosition2().get(i)[1] != chosenMove.getQueenPosition2().get(i)[1]) {
-//					newQuen = chosenMove.getQueenPosition2().get(i);
-//					break;
-//				}
-//		}
-//		System.out.println("NewQinMeth: " + newQuen[0] + ";" + newQuen[1]);
-//		return newQuen;
-//	}
-//
-//	// looking for the new arrow that was thrown
-//	private static int[] getArrow(Node root, Node chosenMove) {
-//		int[] arrow = new int[2];
-//		// get arrow from new game state
-//		for (int x = 0; x < chosenMove.getArrowPositions().size(); x++) {
-//			// if our newest arrow is last in list grab it return it
-//			if (x == chosenMove.getArrowPositions().size() - 1) {
-//				arrow = chosenMove.getArrowPositions().get(x);
-//				break;
-//			}
-//			
-//			// otherwise loop through until we find the arrow pair thats different
-//			if (chosenMove.getArrowPositions().get(x)[0] != root.getArrowPositions().get(x)[0]||
-//				chosenMove.getArrowPositions().get(x)[1] != root.getArrowPositions().get(x)[1]) {
-//					arrow = chosenMove.getArrowPositions().get(x);
-//					break;
-//				
-//			}
-//		}
-//		return arrow;
-//	}
+
 
 }
