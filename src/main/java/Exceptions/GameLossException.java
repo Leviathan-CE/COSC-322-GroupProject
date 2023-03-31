1617e029b87b35aea07fa1b7af4ed718e51b3633
Leviathan <leviathancreative@outlook.com>
2023-03-31 13:52:00 -0700

    win loss messages

--- src/main/java/Exceptions/GameLossException.java
+++ src/main/java/Exceptions/GameLossException.java
@@ -7,16 +7,17 @@ public class GameLossException 	extends RuntimeException{
 		 */
 		private static final long serialVersionUID = 1L;
 		public GameLossException(String Message) {
-			super("");
-			System.out.println("\u001B[31m --------------------------");
-			System.out.println("\u001B[31m --------------------------");
-			System.out.println("\u001B[31m --------------------------");
-			System.out.println("\u001B[31m --------------------------");
-			System.out.println("\u001B[31m -------"+Message+"-----");
-			System.out.println("\u001B[31m --------------------------");
-			System.out.println("\u001B[31m --------------------------");
-			System.out.println("\u001B[31m --------------------------");
-			System.out.println("\u001B[31m --------------------------");
+			super(Message);
+			System.out.println("\\u001B[31m ------------------------------------------------");
+			System.out.println("\\u001B[31m ------------------------------------------------");
+			System.out.println("\\u001B[31m ------------------------------------------------");
+			System.out.println("\\u001B[31m ------------------------------------------------");
+			System.out.println("\\u001B[31m ------------------------------------------------");
+			System.out.println("\\u001B[31m ----------------"+Message+"---------------------");
+			System.out.println("\\u001B[31m ------------------------------------------------");
+			System.out.println("\\u001B[31m ------------------------------------------------");
+			System.out.println("\\u001B[31m ------------------------------------------------");
+			System.out.println("\\u001B[31m ------------------------------------------------");
 		}
 
 }
--- src/main/java/Exceptions/GameWinException.java
+++ src/main/java/Exceptions/GameWinException.java
@@ -6,17 +6,19 @@ public class GameWinException extends RuntimeException{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public GameWinException(String Message) {
-		super("");
-		System.out.println("\u001B[32m --------------------------");
-		System.out.println("\u001B[32m --------------------------");
-		System.out.println("\u001B[32m --------------------------");
-		System.out.println("\u001B[32m --------------------------");
-		System.out.println("\u001B[32m -------"+Message+"-----");
-		System.out.println("\u001B[32m --------------------------");
-		System.out.println("\u001B[32m --------------------------");
-		System.out.println("\u001B[32m --------------------------");
-		System.out.println("\u001B[32m --------------------------");
+	public GameWinException(String Message) {		
+		super(Message);
+		System.out.println("\\u001B[32m ------------------------------------------------");
+		System.out.println("\\u001B[32m ------------------------------------------------");
+		System.out.println("\\u001B[32m ------------------------------------------------");
+		System.out.println("\\u001B[32m ------------------------------------------------");
+		System.out.println("\\u001B[32m ------------------------------------------------");
+		System.out.println("\\u001B[32m ----------------"+Message+"---------------------");
+		System.out.println("\\u001B[32m ------------------------------------------------");
+		System.out.println("\\u001B[32m ------------------------------------------------");
+		System.out.println("\\u001B[32m ------------------------------------------------");
+		System.out.println("\\u001B[32m ------------------------------------------------");
+
 	}
 
 }
